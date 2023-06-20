a759b5eba12e960b0e5ee2709b2c27e527a5c8c0
Benjamin Copen <pinj2000@gmail.com>
2023-06-20 17:49:32 -0400

    first commit

---
+++ W2_D1/code_along/py_whiteboard.py
@@ -0,0 +1,24 @@
+# Title: Counting Strings Exercise
+
+# Problem Statement:
+# You are given two strings, A and B. Your task is to count how many strings equal to A can be constructed using letters from the string B. 
+# Each letter can be used only once and in one string only.
+
+# Example:
+# Let's take an example to understand the problem better:
+# A = "abc"
+# B = "abccba"
+
+
+# Task:
+# Your task is to write a Python function that takes in two strings, A and B, and returns the count of strings equal to A that can be constructed using letters from B.
+
+
+
+# Input:
+
+# A: A string containing lowercase letters. (3 ≤ A.length ≤ 9)
+# B: A string containing lowercase letters. (3 ≤ B.length ≤ 50)
+# Output:
+
+# An integer representing the count of strings equal to A that can be constructed using letters from B.
